8d29a8c43cf06ece7d8173d644e15af24ab2481c
snayeemu <snayeemu2@gmail.com>
2023-08-05 00:59:41 +0600

    completed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ttlimcj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,8 +32,58 @@ async function run() {
     app.get("/tasks/:email", async (req, res) => {
       const email = req.params.email;
       const query = { user: email };
-      console.log(email);
       const result = await tasksCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+    app.delete("/delete/:_id", async (req, res) => {
+      const _id = req.params._id;
+      const query = { _id: new ObjectId(_id) };
+      const result = await tasksCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/update/:_id", async (req, res) => {
+      const _id = req.params._id;
+      const query = { _id: new ObjectId(_id) };
+      let task = await tasksCollection.findOne(query);
+      task = {
+        $set: {
+          status: `completed`,
+        },
+      };
+
+      const result = await tasksCollection.updateOne(query, task);
+      res.send(result);
+    });
+
+    app.post("/add-task", async (req, res) => {
+      const newTask = req.body;
+      const result = await tasksCollection.insertOne(newTask);
+
+      res.send(result);
+    });
+
+    app.patch("/update-task/:_id", async (req, res) => {
+      const _id = req.params._id;
+      const query = { _id: new ObjectId(_id) };
+      const updatedTask = req.body;
+
+      let oldTask = await tasksCollection.findOne(query);
+
+      if (!oldTask) {
+        return "not this task found";
+      }
+
+      oldTask = {
+        $set: {
+          title: updatedTask.title,
+          description: updatedTask.description,
+        },
+      };
+
+      const result = await tasksCollection.updateOne(query, oldTask);
 
       res.send(result);
     });
